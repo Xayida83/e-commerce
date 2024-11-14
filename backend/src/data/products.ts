df34e38bc190eb73a2dc06a12291029431143d4c
Xayida83 <lotta.lindberg83@gmail.com>
2024-11-14 20:41:18 +0100

    added products and gitignore for backend

--- backend/src/data/products.ts
+++ backend/src/data/products.ts
@@ -1,24 +1,274 @@
 import { IProduct } from "../models/IProduct";
 
 export const products: IProduct[] = [
-  { id: 1, name: 'Christmas Tree', price: 499, description: 'A 6-foot artificial Christmas tree with built-in LED lights.', stock: 10, category: 'Decorations', imageUrl: 'https://images.clasohlson.com/medias/sys_master/he1/he5/68450077835294.jpg' },
-  { id: 2, name: 'Christmas Wreath', price: 29, description: 'A beautiful Christmas wreath with red and gold accents.', stock: 15, category: 'Decorations', imageUrl: 'https://cdn.printerval.com/unsafe/960x960/asset.prtvstatic.com/2022/11/21/avga-b0955d184582592fcf2595e0cdbd0e1b.jpg' },
-  { id: 3, name: 'Set of 12 Christmas Ornaments', price: 12, description: 'A set of 12 glass ornaments in various colors.', stock: 20, category: 'Decorations', imageUrl: 'https://www.cchobby.se/media/catalog/product/cache/f5a04654fa1291930ec6413dd4db78d6/5/2/52165_1.jpg' },
-  { id: 4, name: 'Christmas Stocking', price: 10, description: 'A classic red Christmas stocking with white fur trim.', stock: 50, category: 'Decorations', imageUrl: 'https://cdn.lyckasmedmat.se/57817-large_default/klassisk-julstrumpa-roed-vit.jpg' },
-  { id: 5, name: 'String Lights', price: 19, description: '10 meters of warm white LED string lights.', stock: 30, category: 'Lights', imageUrl: 'https://assets.holyart.it/images/PR013550/en/500/R/SN056196/CLOSEUP01_HD/h-2b0acdab/christmas-lights-10-m-100-led-drop-lights-warm-white-indoor-use.jpg' },
-  { id: 6, name: 'Tree Topper Star', price: 15, description: 'A golden star for the top of the Christmas tree.', stock: 18, category: 'Decorations', imageUrl: 'https://www.officedepot.se/-/media/commerce/sweden/original/85/60/8560330.jpg?bc=white&h=370&w=370&hash=EC502FC9B5EC375CC602502506878803398C887F' },
-  { id: 7, name: 'Christmas Table Runner', price: 25, description: 'A festive red table runner with Christmas motifs.', stock: 10, category: 'Decorations', imageUrl: 'https://ae-pic-a1.aliexpress-media.com/kf/S4144bd0cceb544658341712dd45cb6cfh/Merry-Christmas-Red-Pine-Trees-Linen-Table-Runner-Winter-Xmas-Holiday-Party-Kitchen-Dining-Table-Decor.jpg_.webp' },
-  { id: 8, name: 'Santa Claus Figurine', price: 35, description: 'A 12-inch Santa Claus figurine.', stock: 12, category: 'Decorations', imageUrl: 'https://shop.cramersblommor.com/jul/tomte-djur-figurer/tomte-rod-10x20x30cm/?gad_source=1&gclid=CjwKCAiAudG5BhAREiwAWMlSjMV3lSqtmRSGR-PH4N4bu1Yo903LZndF9ksI7eYMDA1-VOtI5CNEKhoCct0QAvD_BwE' },
-  { id: 9, name: 'LED Snow Globe', price: 20, description: 'A snow globe with LED lighting and snow effect.', stock: 15, category: 'Decorations', imageUrl: 'https://assets.cdn.jula.com/preset:jpgoptimized/w:828/Dmm3BWSV3/assetstream.aspx?assetId=208479&mediaformatid=50137&destinationid=10016&lastmodified=20240110103022' },
-  { id: 10, name: 'Christmas Candle Set', price: 18, description: 'A set of three Christmas-scented candles.', stock: 22, category: 'Candles', imageUrl: 'https://cdn1.jysk.com/getimage/wd3.large/129338' },
-  { id: 11, name: 'Reindeer Figurine', price: 22, description: 'A wooden reindeer figurine for your mantel.', stock: 17, category: 'Decorations', imageUrl: 'https://t4.ftcdn.net/jpg/10/40/28/73/360_F_1040287309_P3jfQfA0vEOdLqcPA57etBz6RHdxXp10.jpg' },
-  { id: 12, name: 'Holiday Door Mat', price: 12, description: 'A doormat with a festive holiday design.', stock: 25, category: 'Decorations', imageUrl: 'https://www.rusta.com/cdn-cgi/image/quality=80,width=848,format=auto/globalassets/productimages/454018040101-12-p.jpg?ref=25688F895A' },
-  { id: 13, name: 'Advent Calendar', price: 15, description: 'A reusable advent calendar with 24 pockets.', stock: 20, category: 'Decorations', imageUrl: 'https://img.freepik.com/free-vector/christmas-advent-calendar_23-2147532775.jpg?t=st=1731503985~exp=1731507585~hmac=55789fa831ad350b3f198644cf488cae2e533988ab6178dcb478ecf498d9bcb2&w=740' },
-  { id: 14, name: 'Garland with Berries', price: 16, description: 'A Christmas garland with faux berries and pine cones.', stock: 15, category: 'Decorations', imageUrl: 'https://img.freepik.com/free-photo/top-view-shot-pine-branches-with-cone-gift-black-background_181624-26765.jpg?t=st=1731503895~exp=1731507495~hmac=7cb71cf3eee3f32585388374f144bab763aad615ebdb6b6f133c31dbbfa303ad&w=996' },
-  { id: 15, name: 'Elf Shelf Doll', price: 10, description: 'A festive elf doll for the shelf.', stock: 35, category: 'Decorations', imageUrl: 'https://img.freepik.com/free-photo/funny-garden-gnome-outside_23-2149136679.jpg?t=st=1731504088~exp=1731507688~hmac=d5c238be515b6a5a81c8a03362dc0383c5a0caa62437a75702582cb53dffb7c5&w=360' },
-  { id: 16, name: 'Nutcracker Soldier', price: 30, description: 'A classic nutcracker soldier figurine.', stock: 10, category: 'Decorations', imageUrl: 'https://img.freepik.com/free-photo/traditional-nutcracker-figure-isolated-white-background_123827-20016.jpg?t=st=1731504124~exp=1731507724~hmac=f40bc80efefcb942b9e7da4b4bdc0534dfaf2298c34577083744a126f9f2f513&w=996' },
-  { id: 17, name: 'Christmas Tree Skirt', price: 20, description: 'A red and white Christmas tree skirt.', stock: 25, category: 'Decorations', imageUrl: 'https://raw.githubusercontent.com/Xayida83/e-commerce/refs/heads/main/src/assets/christmas_mat.webp' },
-  { id: 18, name: 'Christmas Cookie Cutter Set', price: 8, description: 'A set of 5 cookie cutters in festive shapes.', stock: 50, category: 'Kitchen', imageUrl: 'https://img.freepik.com/free-vector/hand-drawn-christmas-chocolates-collection_23-2149848429.jpg?t=st=1731504435~exp=1731508035~hmac=3d48a51b3bdb8871261bf14847cceea018ca567d0926556c36bd7a8a232d971c&w=740' },
-  { id: 19, name: 'Pine Scented Candle', price: 12, description: 'A candle with a fresh pine scent for the holidays.', stock: 20, category: 'Candles', imageUrl: 'https://img.freepik.com/free-photo/christmas-composition-candle-candlestick_23-2147955729.jpg?t=st=1731504471~exp=1731508071~hmac=9c4f046ce66decde809a618932e742fa955c9ce312434578abc3e641d3439c76&w=996' },
-  { id: 20, name: 'Christmas Gift Wrap Set', price: 15, description: 'A set with wrapping paper, ribbons, and gift tags.', stock: 40, category: 'Gift Wrap', imageUrl: 'https://img.freepik.com/free-photo/festive-still-life-christmas-present-arrangement_23-2149161144.jpg?t=st=1731504538~exp=1731508138~hmac=fd35cb4706c4bf224733d16dd7f21ad21ffbd4de72cb4d262aff2033eb2503c2&w=996' }
+  {
+    id: 1,
+    name: 'Christmas Tree',
+    price: 499,
+    description: 'A 6-foot artificial Christmas tree with built-in LED lights.',
+    stock: 10,
+    category: 'Decorations',
+    imageUrl: 'https://images.clasohlson.com/medias/sys_master/he1/he5/68450077835294.jpg'
+  },
+  {
+    id: 2,
+    name: 'Christmas Wreath',
+    price: 29,
+    description: 'A beautiful Christmas wreath with red and gold accents.',
+    stock: 15,
+    category: 'Decorations',
+    imageUrl: 'https://cdn.printerval.com/unsafe/960x960/asset.prtvstatic.com/2022/11/21/avga-b0955d184582592fcf2595e0cdbd0e1b.jpg'
+  },
+  {
+    id: 3,
+    name: 'Set of 12 Christmas Ornaments',
+    price: 12,
+    description: 'A set of 12 glass ornaments in various colors.',
+    stock: 20,
+    category: 'Decorations',
+    imageUrl: 'https://www.cchobby.se/media/catalog/product/cache/f5a04654fa1291930ec6413dd4db78d6/5/2/52165_1.jpg'
+  },
+  {
+    id: 4,
+    name: 'Christmas Stocking',
+    price: 10,
+    description: 'A classic red Christmas stocking with white fur trim.',
+    stock: 50,
+    category: 'Decorations',
+    imageUrl: 'https://cdn.lyckasmedmat.se/57817-large_default/klassisk-julstrumpa-roed-vit.jpg'
+  },
+  {
+    id: 5,
+    name: 'String Lights',
+    price: 19,
+    description: '10 meters of warm white LED string lights.',
+    stock: 30,
+    category: 'Lights',
+    imageUrl: 'https://assets.holyart.it/images/PR013550/en/500/R/SN056196/CLOSEUP01_HD/h-2b0acdab/christmas-lights-10-m-100-led-drop-lights-warm-white-indoor-use.jpg'
+  },
+  {
+    id: 6,
+    name: 'Tree Topper Star',
+    price: 15,
+    description: 'A golden star for the top of the Christmas tree.',
+    stock: 18,
+    category: 'Decorations',
+    imageUrl: 'https://www.officedepot.se/-/media/commerce/sweden/original/85/60/8560330.jpg?bc=white&h=370&w=370&hash=EC502FC9B5EC375CC602502506878803398C887F'
+  },
+  {
+    id: 7,
+    name: 'Christmas Table Runner',
+    price: 25,
+    description: 'A festive red table runner with Christmas motifs.',
+    stock: 10,
+    category: 'Decorations',
+    imageUrl: 'https://ae-pic-a1.aliexpress-media.com/kf/S4144bd0cceb544658341712dd45cb6cfh/Merry-Christmas-Red-Pine-Trees-Linen-Table-Runner-Winter-Xmas-Holiday-Party-Kitchen-Dining-Table-Decor.jpg_.webp'
+  },
+  {
+    id: 8,
+    name: 'Santa Claus Figurine',
+    price: 35,
+    description: 'A 12-inch Santa Claus figurine.',
+    stock: 12,
+    category: 'Decorations',
+    imageUrl: 'https://shop.cramersblommor.com/jul/tomte-djur-figurer/tomte-rod-10x20x30cm/?gad_source=1&gclid=CjwKCAiAudG5BhAREiwAWMlSjMV3lSqtmRSGR-PH4N4bu1Yo903LZndF9ksI7eYMDA1-VOtI5CNEKhoCct0QAvD_BwE'
+  },
+  {
+    id: 9,
+    name: 'LED Snow Globe',
+    price: 20,
+    description: 'A snow globe with LED lighting and snow effect.',
+    stock: 15,
+    category: 'Decorations',
+    imageUrl: 'https://assets.cdn.jula.com/preset:jpgoptimized/w:828/Dmm3BWSV3/assetstream.aspx?assetId=208479&mediaformatid=50137&destinationid=10016&lastmodified=20240110103022'
+  },
+  {
+    id: 10,
+    name: 'Christmas Candle Set',
+    price: 18,
+    description: 'A set of three Christmas-scented candles.',
+    stock: 22,
+    category: 'Candles',
+    imageUrl: 'https://cdn1.jysk.com/getimage/wd3.large/129338'
+  },
+  {
+    id: 11,
+    name: 'Reindeer Figurine',
+    price: 22,
+    description: 'A wooden reindeer figurine for your mantel.',
+    stock: 17,
+    category: 'Decorations',
+    imageUrl: 'https://t4.ftcdn.net/jpg/10/40/28/73/360_F_1040287309_P3jfQfA0vEOdLqcPA57etBz6RHdxXp10.jpg'
+  },
+  {
+    id: 12,
+    name: 'Holiday Door Mat',
+    price: 12,
+    description: 'A doormat with a festive holiday design.',
+    stock: 25,
+    category: 'Decorations',
+    imageUrl: 'https://www.rusta.com/cdn-cgi/image/quality=80,width=848,format=auto/globalassets/productimages/454018040101-12-p.jpg?ref=25688F895A'
+  },
+  {
+    id: 13,
+    name: 'Advent Calendar',
+    price: 15,
+    description: 'A reusable advent calendar with 24 pockets.',
+    stock: 20,
+    category: 'Decorations',
+    imageUrl: 'https://img.freepik.com/free-vector/christmas-advent-calendar_23-2147532775.jpg?t=st=1731503985~exp=1731507585~hmac=55789fa831ad350b3f198644cf488cae2e533988ab6178dcb478ecf498d9bcb2&w=740'
+  },
+  {
+    id: 14,
+    name: 'Garland with Berries',
+    price: 16,
+    description: 'A Christmas garland with faux berries and pine cones.',
+    stock: 15,
+    category: 'Decorations',
+    imageUrl: 'https://img.freepik.com/free-photo/top-view-shot-pine-branches-with-cone-gift-black-background_181624-26765.jpg?t=st=1731503895~exp=1731507495~hmac=7cb71cf3eee3f32585388374f144bab763aad615ebdb6b6f133c31dbbfa303ad&w=996'
+  },
+  {
+    id: 15,
+    name: 'Elf Shelf Doll',
+    price: 10,
+    description: 'A festive elf doll for the shelf.',
+    stock: 35,
+    category: 'Decorations',
+    imageUrl: 'https://img.freepik.com/free-photo/funny-garden-gnome-outside_23-2149136679.jpg?t=st=1731504088~exp=1731507688~hmac=d5c238be515b6a5a81c8a03362dc0383c5a0caa62437a75702582cb53dffb7c5&w=360'
+  },
+  {
+    id: 16,
+    name: 'Nutcracker Soldier',
+    price: 30,
+    description: 'A classic nutcracker soldier figurine.',
+    stock: 10,
+    category: 'Decorations',
+    imageUrl: 'https://img.freepik.com/free-photo/traditional-nutcracker-figure-isolated-white-background_123827-20016.jpg?t=st=1731504124~exp=1731507724~hmac=f40bc80efefcb942b9e7da4b4bdc0534dfaf2298c34577083744a126f9f2f513&w=996'
+  },
+  {
+    id: 17,
+    name: 'Christmas Tree Skirt',
+    price: 20,
+    description: 'A red and white Christmas tree skirt.',
+    stock: 25,
+    category: 'Decorations',
+    imageUrl: 'https://raw.githubusercontent.com/Xayida83/e-commerce/refs/heads/main/src/assets/christmas_mat.webp'
+  },
+  {
+    id: 18,
+    name: 'Christmas Cookie Cutter Set',
+    price: 8,
+    description: 'A set of 5 cookie cutters in festive shapes.',
+    stock: 50,
+    category: 'Kitchen',
+    imageUrl: 'https://img.freepik.com/free-vector/hand-drawn-christmas-chocolates-collection_23-2149848429.jpg?t=st=1731504435~exp=1731508035~hmac=3d48a51b3bdb8871261bf14847cceea018ca567d0926556c36bd7a8a232d971c&w=740'
+  },
+  {
+    id: 19,
+    name: 'Pine Scented Candle',
+    price: 12,
+    description: 'A candle with a fresh pine scent for the holidays.',
+    stock: 20,
+    category: 'Candles',
+    imageUrl: 'https://img.freepik.com/free-photo/christmas-composition-candle-candlestick_23-2147955729.jpg?t=st=1731504471~exp=1731508071~hmac=9c4f046ce66decde809a618932e742fa955c9ce312434578abc3e641d3439c76&w=996'
+  },
+  {
+    id: 20,
+    name: 'Christmas Gift Wrap Set',
+    price: 15,
+    description: 'A set with wrapping paper, ribbons, and gift tags.',
+    stock: 40,
+    category: 'Gift Wrap',
+    imageUrl: 'https://img.freepik.com/free-photo/festive-still-life-christmas-present-arrangement_23-2149161144.jpg?t=st=1731504538~exp=1731508138~hmac=fd35cb4706c4bf224733d16dd7f21ad21ffbd4de72cb4d262aff2033eb2503c2&w=996'
+  },
+  {
+    id: 21,
+    name: 'Champagne Flutes',
+    price: 25,
+    description: 'Set of 6 elegant champagne flutes, perfect for New Year\'s toasts.',
+    stock: 30,
+    category: 'Glassware',
+    imageUrl: 'https://via.placeholder.com/150'
+  },
+  {
+    id: 22,
+    name: 'Party Hats',
+    price: 10,
+    description: 'A pack of 10 colorful party hats with glitter details.',
+    stock: 50,
+    category: 'Party Supplies',
+    imageUrl: 'https://via.placeholder.com/150'
+  },
+  {
+    id: 23,
+    name: 'Confetti Cannons',
+    price: 12,
+    description: 'Pack of 3 confetti cannons for a festive New Year\'s celebration.',
+    stock: 40,
+    category: 'Party Supplies',
+    imageUrl: 'https://via.placeholder.com/150'
+  },
+  {
+    id: 24,
+    name: 'New Year\'s Eve Balloons',
+    price: 8,
+    description: 'A mix of 20 balloons in black, gold, and silver with "Happy New Year" text.',
+    stock: 60,
+    category: 'Decorations',
+    imageUrl: 'https://via.placeholder.com/150'
+  },
+  {
+    id: 25,
+    name: 'LED String Lights',
+    price: 15,
+    description: '10-meter LED string lights for a festive atmosphere.',
+    stock: 25,
+    category: 'Decorations',
+    imageUrl: 'https://via.placeholder.com/150'
+  },
+  {
+    id: 26,
+    name: 'Disposable Champagne Glasses',
+    price: 10,
+    description: 'Set of 12 plastic champagne glasses, perfect for parties.',
+    stock: 35,
+    category: 'Glassware',
+    imageUrl: 'https://via.placeholder.com/150'
+  },
+  {
+    id: 27,
+    name: 'Countdown Clock',
+    price: 20,
+    description: 'A digital clock to help countdown to the New Year.',
+    stock: 15,
+    category: 'Decorations',
+    imageUrl: 'https://via.placeholder.com/150'
+  },
+  {
+    id: 28,
+    name: 'New Year Banner',
+    price: 8,
+    description: 'A "Happy New Year" banner in gold and black.',
+    stock: 40,
+    category: 'Decorations',
+    imageUrl: 'https://via.placeholder.com/150'
+  },
+  {
+    id: 29,
+    name: 'Sparkling Cider',
+    price: 12,
+    description: 'Non-alcoholic sparkling cider, perfect for toasting.',
+    stock: 30,
+    category: 'Beverages',
+    imageUrl: 'https://via.placeholder.com/150'
+  },
+  {
+    id: 30,
+    name: 'Table Confetti',
+    price: 5,
+    description: 'Gold and silver star-shaped table confetti for decoration.',
+    stock: 50,
+    category: 'Party Supplies',
+    imageUrl: 'https://via.placeholder.com/150'
+  }
 ];
